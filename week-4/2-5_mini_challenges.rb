16d516f05530e8e7b1bc74578abf9fc29f53d7f4
unknown <zolliebarnes@gmail.com>
2015-11-14 19:34:20 -0500

    set-up for 4.2

---
+++ week-4/2-5_mini_challenges.rb
@@ -0,0 +1,12 @@
+#Hours in a year
+
+year = 1
+days = 365
+hours = 24
+
+hours_in_a_year = year*days*hours
+
+#Minutes in a Decade
+
+
+
--- week-4/good-guess/my_solution.rb
+++ week-4/good-guess/my_solution.rb
@@ -11,16 +11,3 @@ Create method called get_grade
 	Return A,B,C etc (no +/-)
 =end
 
-def get_grade(grade)
-	puts "What is the class average?"
-	grade=gets.chomp
-	if grade >= 90
-		return "A"
-	elsif grade >= 80
-		return "B"
-	elsif grade >= 70
-		return "C"
-	elsif grade >= 60
-		return "D"
-	else return "F"
-	end
