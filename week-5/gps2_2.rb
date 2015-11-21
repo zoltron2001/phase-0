07ba0eff93a24f0ca62af0e8bb38afeaad568cfa
unknown <zolliebarnes@gmail.com>
2015-11-21 12:18:42 -0500

    more setup for 2_2

---
+++ week-5/gps2_2.rb
@@ -0,0 +1,39 @@
+=begin
+
+pseudocode
+
+Create a new list
+Add an item with a quantity to the list
+Remove an item from the list
+Update quantities for items in your list
+Print the list (Consider how to make it look nice!)
+
+=end
+
+# Method to create a list
+# input: string of items separated by spaces (example: "carrots apples cereal pizza")
+# steps: 
+  # [fill in any steps here]
+  # set default quantity
+  # print the list to the console [can you use one of your other methods here?]
+# output: [what data type goes here, array or hash?]
+
+# Method to add an item to a list
+# input: item name and optional quantity
+# steps:
+# output:
+
+# Method to remove an item from the list
+# input:
+# steps:
+# output:
+
+# Method to update the quantity of an item
+# input:
+# steps:
+# output:
+
+# Method to print a list and make it look pretty
+# input:
+# steps:
+# output:
