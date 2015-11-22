d57820cea99bd7414b6a32a51693f17dfc284ed6
unknown <zolliebarnes@gmail.com>
2015-11-22 14:15:39 -0500

    finish num/commas and setup gps

--- week-5/gps2_2.rb
+++ week-5/gps2_2.rb
@@ -11,34 +11,35 @@ Remove an item from the list
 Update quantities for items in your list
 Print the list (Consider how to make it look nice!)
 
-pseudocode
-
+pseudocode:
 =end
 
 # Method to create a list
 # input: string of items separated by spaces (example: "carrots apples cereal pizza")
 # steps: 
-  # [fill in any steps here]
-  # set default quantity
+  # split string into array (spaces become commas/new elements)
+  # create new hash with a default quantity of 1
+  # import the array as keys into the hash
+  # add to the quantity if any duplicates are found
   # print the list to the console [can you use one of your other methods here?]
 # output: hash (item, quantity)
 
 # Method to add an item to a list
 # input: item name and optional quantity
-# steps:
-# output:
+# steps: 
+# output: 
 
 # Method to remove an item from the list
-# input:
-# steps:
-# output:
+# input: 
+# steps: 
+# output: 
 
 # Method to update the quantity of an item
-# input:
-# steps:
-# output:
+# input: 
+# steps: 
+# output: 
 
 # Method to print a list and make it look pretty
-# input:
-# steps:
-# output:
+# input: 
+# steps: 
+# output: 
--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -16,6 +16,7 @@
 
 # What are the steps needed to solve the problem?
 =begin
+
 convert number into string
   get length of string
 IF it's 3 characters or shorter output the string
@@ -28,35 +29,18 @@ ELSE split string into characters
   output string
 
 1   2   3   4   5   6   7   8
-0   1   2   3   4   5   6   7
 -8 -7  -6  -5  -4  -3  -2  -1
+
 1   2   3   4   5 , 6   7   8
 -9 -8  -7  -6  -5-4-3  -2  -1
+
 1   2 , 3   4   5 , 6   7   8
 -10 9-8-7  -6  -5-4-3  -2  -1
 
-"1,23,45,678"
-4
 =end
 
 # 1. Initial Solution
-
-def separate_comma(int)
-  output = int.to_s
-  return output if output.length <=3
-  array = output.split("")
-  count = -4
-  total_length = -1*(array.length)
-  until count < total_length
-    array.insert(count, ",")
-    count -= 4
-    total_length -= 1
-  end
-  return array.join
-end
-
-# 2. Refactored Solution
-
+=begin
 def separate_comma(int)
 # output becomes the integer which is converted to a string
   output = int.to_s
@@ -75,7 +59,46 @@ def separate_comma(int)
 # rejoin the array into a single string and output
   return array.join
 end
+=end
+# 2. Refactored Solution
 
-#for further refactoring http://codereview.stackexchange.com/questions/28054/separate-numbers-with-commas
+def separate_comma(int)
+# convert integer to string, then split the characters up into an array
+  array = int.to_s.split("")
+# count from right to left to figure out where commas should go
+  count = -4
+  total_length = -1*(array.length)
+  until count < total_length
+    array.insert(count, ",")
+    count -= 4
+# bump the total length size since we added a comma
+    total_length -= 1
+  end
+# rejoin the array into a single string and output
+  return array.join
+end
 
 # 3. Reflection
+=begin
+
+What was your process for breaking the problem down? What different approaches did you consider?
+
+  Initially, I just wrote out the steps. Inevitably, I got stuck on the "insert comma" bit and had to "draw" it out in my pseudocode.
+
+Was your pseudocode effective in helping you build a successful initial solution?
+
+  Yeah definitely. It wasn't far off from the solution. Some parts were refactored out, but the organization remained the same.
+
+What new Ruby method(s) did you use when refactoring your solution? Describe your experience of using the Ruby documentation to implement it/them (any difficulties, etc.). Did it/they significantly change the way your code works? If so, how?
+
+  I didn't use new methods, I just removed unnecessary lines and re-wrote things in a more succinct way. I did find a more efficient solution involving reversing string, splitting it every third character, and recombining/reversing with a comma inbetween. But it had nothing to do with my ideas, so I felt like I would have been plagerizing. Regardless, I feel like I understand .split, .reverse, and .join now.
+
+How did you initially iterate through the data structure?
+
+  The very first attempts were from left to right. And even when I ended up going from right to left, I was trying to do it by threes, or failing to realize that the array was growing in size.
+
+Do you feel your refactored solution is more readable than your initial solution? Why?
+
+Yeah, I think so, but neither are too bad. There's less to read in the refactored one, and one fewer variable to keep track of.
+
+=end
