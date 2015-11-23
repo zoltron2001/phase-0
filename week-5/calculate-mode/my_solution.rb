d5d22e4c5fd357ade73a3a616bdf6b946f9301df
unknown <zolliebarnes@gmail.com>
2015-11-23 00:55:25 -0500

    update with reflections

--- week-5/calculate-mode/my_solution.rb
+++ week-5/calculate-mode/my_solution.rb
@@ -73,4 +73,28 @@ def mode(array)
 end
 
 # 4. Reflection
+=begin
+Which data structure did you and your pair decide to implement and why?
+  We used a hash. The hash value allowed us to keep track of duplicate keys (we 
+  simply added one to the value). That way, when it came time to assess the most 
+  frequently used key, we could grab the key with the biggest value.
 
+Were you more successful breaking this problem down into implementable pseudocode 
+than the last with a pair? 
+  No. Because this was really only a two step problem, we didn't really rely on 
+  our pseudocode. That wasn't a spoken agreement, it just kind of worked out that 
+  way. We both understood what we had to figured out, and we just researched the 
+  heck out of that.
+
+What issues/successes did you run into when translating your pseudocode to code?
+  We had a lot of trouble accessing and manipulating hash values. A lot of the 
+  confusion had to do with syntax - whether the hash should be called with []'s, 
+  ()'s, "#{}"'s etc. It also took us a while to find the .has_key? method.
+
+What methods did you use to iterate through the content? Did you find any good ones 
+when you were refactoring? Were they difficult to implement?
+  We didn't find any new iterators. And we didn't find anything while refactoring. 
+  However, .has_key? and .values.max were new to us. They weren't difficult to 
+  implement so much as the hash syntax was.
+
+=end 
