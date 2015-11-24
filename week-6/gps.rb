3dfcfd2f029aad3c98a2d193465c17825d2f80c4
unknown <zolliebarnes@gmail.com>
2015-11-23 21:07:58 -0500

    add and finish gps

--- week-6/gps.rb
+++ week-6/gps.rb
@@ -10,17 +10,18 @@ def serving_size_calc(item_to_make, order_quantity)
   
   menu = {"cookie" => 1, "cake" =>  5, "pie" => 7}
 
-  #   set error_counter to three why 3?
+  # raise argument error if requested item is not on menu
   if menu.has_key?(item_to_make) == false
      raise ArgumentError.new("#{item_to_make} is not a valid input")
   end
   
-  # remainder of order_quantity/serving size will be mod? why?
-  serving_size_mod = order_quantity % menu[item_to_make]
+  # determine what (if anything) will be left over from order
+  leftovers = order_quantity % menu[item_to_make]
 
+  # spit out order
   # note left overs if mod has a remainder
-  if serving_size_mod != 0
-    return "Calculations complete: Make #{order_quantity/menu[item_to_make]} of #{item_to_make}, you have #{serving_size_mod} leftover ingredients."
+  if leftovers != 0
+    return "Calculations complete: Make #{order_quantity/menu[item_to_make]} of #{item_to_make}, you have #{leftovers} leftover ingredients."
   else
     return "Calculations complete: Make #{order_quantity/menu[item_to_make]} of #{item_to_make}"
   end
@@ -34,6 +35,23 @@ p serving_size_calc("cake", 7)
 p serving_size_calc("cookie", 1)
 p serving_size_calc("cookie", 10)
 
-# p serving_size_calc("THIS IS AN ERROR", 5)
-
 #  Reflection
+=begin
+What did you learn about making code readable by working on this challenge?
+  The importance of comments were reinfoced. I was able to see the code's 
+  greater function much easier once I had written out a few comments. The guide 
+  also taught me that one-line if-statements were frowned upon if the code 
+  is too long.
+
+Did you learn any new methods? What did you learn about them?
+  Yeah, I learned about .at_value, although it seemed pretty convoluted given 
+  its use.
+
+What did you learn about accessing data in hashes? 
+  Nothing too new here. It was re-affirmed that values can be accessed given 
+  hash[key], however.
+
+What concepts were solidified when working through this challenge?
+  Minimalism and commenting. This code is so much easier to understand now. 
+
+=end
