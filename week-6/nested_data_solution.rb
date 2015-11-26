ed6e48dc96b724bbc5370df23e58f2adccf262dd
unknown <zolliebarnes@gmail.com>
2015-11-25 19:56:20 -0500

    finish 6.5 pair

--- week-6/nested_data_solution.rb
+++ week-6/nested_data_solution.rb
@@ -5,6 +5,8 @@
 array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
 
 # attempts:
+# p array[1][2][0]
+p array[1][1][2][0]
 # ============================================================
 
 
@@ -17,6 +19,8 @@ array = [[1,2], ["inner", ["eagle", "par", ["FORE", "hook"]]]]
 hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
 
 # attempts:
+# hash[outer:][inner:]["almost"][3]
+p hash[:outer][:inner]["almost"][3]
 # ============================================================
 
 
@@ -30,6 +34,7 @@ hash = {outer: {inner: {"almost" => {3 => "congrats!"}}}}
 nested_data = {array: ["array", {hash: "finished"}]}
 
 # attempts:
+p nested_data[:array][1][:hash]
 # ============================================================
 
 
@@ -40,8 +45,41 @@ nested_data = {array: ["array", {hash: "finished"}]}
 
 number_array = [5, [10, 15], [20,25,30], 35]
 
-
+number_array.map! do |x|
+  if x.kind_of?(Array)
+    x.map! do |y|
+      y += 5
+    end
+  else
+    x += 5
+  end
+end
+p number_array
 
 # Bonus:
 
 startup_names = ["bit", ["find", "fast", ["optimize", "scope"]]]
+
+startup_names.map! do |name|
+  #   1st level array values as a name
+  if name.kind_of?(Array)
+    #     Check if 1st level value is an array and if it is
+    name.map! do |also_name|
+      #       Map the second level array values as also_name
+      if also_name.kind_of?(Array)
+        #         Check if second level value is an array and if it is
+        also_name.map! do |also_also_name|
+          # Map the third level value as also_also_name
+          also_also_name += "ly"
+          # Add ly to also also name
+        end
+        # End the name
+      else
+        also_name += "ly"
+      end
+    end 
+  else
+    name += "ly"
+  end
+end
+p startup_names
