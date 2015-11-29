64fe5ce27ff30ee786112da049d71012d3d221ec
unknown <zolliebarnes@gmail.com>
2015-11-28 21:34:50 -0500

    start on bingo solution

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -1,34 +1,76 @@
 # A Nested Array to Model a Bingo Board SOLO CHALLENGE
 
-# I spent [#] hours on this challenge.
-
+# I spent [.5] hours on this challenge.
 
 # Release 0: Pseudocode
 # Outline:
+# => initialize
+# =>   initialize input array
+# =>   initialize an array of BINGO letters
+# => call_spaces
+# =>   randomly generate letter (BINGO)
+# =>   randomly generate number (1-100)
+# =>   call letter and number in single string
+# => check_board
+# =>   use generated letter to determine column
+# =>   check if corresponding number is on the board
+# =>   if it is, replace that number with an 'X'
+# => display_board
+# =>   display new board
+# =>   try to remove the array brackets and commas
+# =>   bonus: add characters like dots to frame
 
-# Create a method to generate a letter ( b, i, n, g, o) and a number (1-100)
-  #fill in the outline here
-
-# Check the called column for the number called.
-  #fill in the outline here
-
-# If the number is in the column, replace with an 'x'
-  #fill in the outline here
-
-# Display a column to the console
-  #fill in the outline here
-
-# Display the board to the console (prettily)
-  #fill in the outline here
+# board = [[00, 01, 02, 03, 04],
+#         [10, 11, 12, 13, 14],
+#         [20, 21, 22, 23, 24],
+#         [30, 31, 32, 33, 34],
+#         [40, 41, 42, 43, 44]]
 
 # Initial Solution
 
 class BingoBoard
-
+  
   def initialize(board)
     @bingo_board = board
+    @bingo = ["B","I","N","G","O"]
+    @column = 0
   end
 
+  def call_spaces
+    # randomly generate letter (BINGO)
+       @letter = @bingo.shuffle[0]
+    # randomly generate number (1-100)
+       @number = rand(1..100)
+    # call combination of letter and number
+       p "The next space is " + "#{@letter}" + "#{@number}... " + "#{@letter}" + "#{@number}" + "."
+
+  end
+
+  def check_board
+    # use generated letter to determine column number
+      if @letter == "B"
+        @column == 0
+      elsif @letter == "I"
+        @column == 1
+      elsif @letter == "N"
+        @column == 2
+      elsif @letter == "G"
+        @column == 3
+      else
+        @column = 4
+      end
+    # check if corresponding number is on the board
+      bingo_transpose = @bingo_board.transpose
+      require 'matrix'
+      mat = Matrix[*bingo_transpose]
+      mat.column[@column].to_a
+      mat.include?(@number)
+    # if it is, replace that number with an 'X'
+  end
+
+  def display_board
+    
+  end
 
 end
 
@@ -37,6 +79,7 @@ end
 
 
 #DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
+
 board = [[47, 44, 71, 8, 88],
         [22, 69, 75, 65, 73],
         [83, 85, 97, 89, 57],
