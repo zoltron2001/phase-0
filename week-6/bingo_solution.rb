fed8b923bd0481007f7342794a0630c9b436603f
unknown <zolliebarnes@gmail.com>
2015-11-29 20:07:20 -0500

    bingo with final solution

--- week-6/bingo_solution.rb
+++ week-6/bingo_solution.rb
@@ -1,6 +1,6 @@
 # A Nested Array to Model a Bingo Board SOLO CHALLENGE
 
-# I spent [.5] hours on this challenge.
+# I spent [3] hours on this challenge.
 
 # Release 0: Pseudocode
 # Outline:
@@ -28,45 +28,95 @@
 
 # Initial Solution
 
-class BingoBoard
+# class BingoBoard
   
-  def initialize(board)
-    @bingo_board = board
-    @bingo = [0,1,2,3,4]
-    @bingo_letters = { 0: "B", 1: "I", 2: "N", 3: "G", 4: "O" }
-    @column = 0
-  end
+#   def initialize(board)
+#     @bingo_board = board.map do |array| 
+#       array.map {|element| element = element.to_s}
+#     end
+#     @bingo = [0,1,2,3,4]
+#     @bingo_letters = {0 => "B", 1 => "I", 2 => "N", 3 => "G", 4 => "O" }
+#     @column = 0
+#   end
 
-  def call_spaces
-    # randomly generate letter (BINGO)
-       @letter = @bingo.shuffle[0]
-    # randomly generate number (1-100)
-       @number = rand(1..100)
-    # call combination of letter and number
-       p "The next space is " + "#{@letter}" + "#{@number}... " + "#{@letter}" + "#{@number}" + "."
+#   def call_spaces
+#      @letter_number = @bingo.shuffle[0]
+#      @letter = @bingo_letters[@letter_number]
+#      @number = rand(1..100).to_s
+#      p "The next space is "+@letter+"-"+@number+"... "+@letter+"-"+@number+"."
+#   end
 
-  end
+#   def check_board
+#     @column = @bingo_letters[@letter]
+#     @bingo_board.map do |element|
+#       if element[@letter_number] == @number
+#         element[@letter_number] = "X"
+#       end
+#     end
+#   end
 
-  def check_board
-    # use generated letter to determine column number
-      @column = @bingo_letters[@letter]
-    # check if corresponding number is on the board
-      @bingo_board.map do |element|
-        if element[@letter] == @number
-    # if it is, replace that number with an 'X'
-          element[@letter] = "X"
-      end
-  end
+#   def display_board
+#     p @bingo_board[0]
+#     p @bingo_board[1]
+#     p @bingo_board[2]
+#     p @bingo_board[3]
+#     p @bingo_board[4]
+#   end
 
-  def display_board
-    return bingo_board
-  end
-
-end
+# end
 
 # Refactored Solution
 
+class BingoBoard
+  
+  def initialize(board)
+    @bingo_letters = {0 => "B", 1 => "I", 2 => "N", 3 => "G", 4 => "O" }
+    # turn all the numbers on the board into strings
+    @bingo_board = board.map do |array| 
+      array.map {|element| element = element.to_s}
+    end
+    # put a space after "8" since it's a single digit number
+    # conditional is incase someone tries this with a new board
+    @bingo_board[0][3] = "8 " if @bingo_board[0][3] == "8"
+    # flavor
+    @dry_remark = ["You won't believe this one. ","Oh wow, ", "Hey look at that, ", "Now we're having fun. ", "Don't spend it all in one place. ", "Is it getting hot in here? "]
+  end
 
+  def call_spaces
+    # generate letter (BINGO)
+     @letter_number = rand(0..4)
+     @letter = @bingo_letters[@letter_number]
+    # generate number (1-100)
+     @number = rand(1..100).to_s
+    # generate remark
+     @dry_remark.shuffle!
+    # print letter, number, remark
+     puts
+     puts " "+@dry_remark[0]+@letter+"-"+@number+"... "+@letter+"-"+@number+"."
+  end
+
+  def check_board
+    # check if corresponding number is on the board
+    # if it is, replace that number with an 'X'
+    @bingo_board.map do |element|
+      if element[@letter_number] == @number
+        element[@letter_number] = "X "
+      end
+    end
+  end
+
+  def display_board
+    # print elements and add spaces to array
+    puts
+    puts "  "+@bingo_board[0].join("  ")
+    puts "  "+@bingo_board[1].join("  ")
+    puts "  "+@bingo_board[2].join("  ")
+    puts "  "+@bingo_board[3].join("  ")
+    puts "  "+@bingo_board[4].join("  ")
+    puts
+  end
+
+end
 
 #DRIVER CODE (I.E. METHOD CALLS) GO BELOW THIS LINE
 
@@ -78,6 +128,9 @@ board = [[47, 44, 71, 8, 88],
 
 new_game = BingoBoard.new(board)
 
+new_game.call_spaces
+new_game.check_board
+new_game.display_board
 
 #Reflection
 
