bd2643f33e2e805b3a3dc435e6af4cce9bd8d929
Zollie Barnes <zolliebarnes@gmail.com>
2015-12-16 17:58:07 -0600

    finish first pair of week

--- week-9/jquery/jquery_example.js
+++ week-9/jquery/jquery_example.js
@@ -13,25 +13,45 @@ $(document).ready(function(){
 
   //Link this script and the jQuery library to the jQuery_example.html file and analyze what this code does.
 
-$('body').css({'background-color': 'pink'})
+  $('body').css({'background-color': 'pink'});
 
 //RELEASE 2:
-  //Add code here to select elements of the DOM
 
+  bodyElement = $('body');
+  mascot = $('.mascot');
+  id_logo = $('#logo');
 
 //RELEASE 3:
-  // Add code here to modify the css and html of DOM elements
 
+  // h1Body = $('body > h1');
+  // h1Body.css('color', 'red');
+  // h1Body.css('border', 'solid 5px');
+  // h1Body.css('visibility', 'hidden');
+  // mascot.html('<h1>Chrous Frogs</h1>');
 
 //RELEASE 4: Event Listener
-  // Add the code for the event listener here
-
+  // var mouseEnter = $('img').on('mouseover', function(e){
+  //     e.preventDefault()
+  //     $(this).attr('src', 'http://media.giphy.com/media/zGJEdp5JzN7JS/giphy-tumblr.gif')
+  //   })
+  //   $('img').on('mouseleave', function(e){
+  //     e.preventDefault()
+  //     $(this).attr('src', 'devbootcamp-logo-new.png')
+  //   });
 
 //RELEASE 5: Experiment on your own
 
+  $("img").css({'border': '1px solid red'})
 
+  $( "img" ).click(function(e) {
+  e.preventDefault()
+  $(this).animate({
+     width: '500px',
+     height: '500px',
+     'border-width': '10px'
+   }, 5000);
+  });
 
+})
 
-
-
-})  // end of the document.ready function: do not remove or write DOM manipulation below this.
+// end of the document.ready function: do not remove or write DOM manipulation below this.
